237bf161be162588cab5a4c05820ddd8ced81ea3
arthurlima13 <lima.arthur02@escola.pr.gov.br>
2026-08-31 09:11:37 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+const desafios =[
+    "Resolver problemas de lógica",
+    "Apreder novas funções",
+    "Criar novas soluções",
+    "Aprender Padrões", 
+    "Criar uma invenção"
+];
+
+function iniciarDesafio() {
+    const name=document.getElementById("name")
+}
